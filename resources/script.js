9b756267120b3ea0b70934843ced1d52592cb27d
Ana Prahova <anastasiaprahova@gmail.com>
2022-09-07 19:40:53 +0300

    added popup video

--- resources/script.js
+++ resources/script.js
@@ -28,17 +28,47 @@ const menuBurgerIcon = document.querySelector('.header-burger-icon');
 const menuBurgerIconsOpened = document.querySelector('.header-burger-icon-opened');
 const menuBurgerIconClose = document.querySelector('.header-burger-icon-close-button');
 const menuBurger = document.querySelector(".header-burger-body");
+const body = document.querySelector(('body'));
 
 if (menuBurgerIcon) {
 	menuBurgerIcon.addEventListener('click', function (e) {
 		menuBurger.classList.toggle("_active");
-		document.body.classList.toggle("_lock");
+		body.classList.toggle("_lock");
 		menuBurgerIconsOpened.classList.toggle("_active");
 		menuBurgerIcon.classList.toggle("_active");
 		menuBurgerIconClose.classList.toggle("_active");
 	});
 
 }
+
+
+// YOUTUBE POP UP WINDOW
+const playYoutube = document.querySelector(".play-icon");
+const popupYoutube = document.querySelector(".pop-up-youtube");
+const popupClose = document.querySelector(".pop-up-close");
+const popupBody = document.querySelector(".pop-up-body");
+const videoElement = document.querySelector('iframe');
+
+if (playYoutube) {
+	playYoutube.addEventListener('click', function (e) {
+		popupYoutube.classList.toggle("open");
+		body.classList.toggle("_lock");
+		videoElement.src = "https://www.youtube.com/embed/f1U9jheilKI?autoplay=1";
+	})
+}
+
+if (popupBody) {
+  popupBody.addEventListener("click", function (e) {
+    popupBody.closest(".pop-up-youtube").classList.toggle("open");
+    videoElement.src = "";
+  });
+}
+
+
+	
+
+
+
 
 
 
